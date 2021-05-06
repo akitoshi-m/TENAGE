d3088c75e417e267435c28308b31a6192ee47243
nexceed <matsuda.a@nexceed.com>
2021-05-06 23:59:25 +0900

    [Add] トップページの文字を編集

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -2,5 +2,34 @@
   <div>
     <TopHeader></TopHeader>
     <h3>トップ画面</h3>
+    <div>
+      <div>
+        <div>
+          <span class="top-text">MOTIVATE</span>
+        </div>
+        <div>
+          <span class="top-text">YOUR LIFE</span>
+        </div>
+        <div>
+          <span class="top-text">AND</span>
+        </div>
+        <div>
+          <span class="top-text">ENERGIZE YOU</span>
+        </div>
+        <v-btn
+          color="#E6855E"
+          outlined
+          text
+          x-large
+        >Tap to START!!</v-btn>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.top-text {
+  color: #E6855E;
+  font-size: 50px;
+}
+</style>
