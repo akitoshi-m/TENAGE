885b0b9c0b6cc117ea1179af2125d4a4a76f5b6f
nexceed <matsuda.a@nexceed.com>
2021-05-08 18:41:03 +0900

    [Add] オプションページの要素を作成

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,6 +1,45 @@
 <template>
   <div>
     <LogoHeader></LogoHeader>
-    <h3>オプション画面</h3>
+    <div class="option">
+      <div class="option-title">
+        
+      </div>
+      <div class="option-menu">
+        <div class="menu-card">
+          <div class="menu-card-title">
+
+          </div>
+          <div class="menu-card-image">
+
+          </div>
+          <div class="menu-card-description">
+
+          </div>
+        </div>
+        <div class="menu-card">
+          <div class="menu-card-title">
+
+          </div>
+          <div class="menu-card-image">
+
+          </div>
+          <div class="menu-card-description">
+            
+          </div>
+        </div>
+        <div class="menu-card">
+          <div class="menu-card-title">
+
+          </div>
+          <div class="menu-card-image">
+
+          </div>
+          <div class="menu-card-description">
+            
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
