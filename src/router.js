e1c4633c0c0ca6d57aae9916b60bb472313d41fc
nexceed <matsuda.a@nexceed.com>
2021-05-08 22:22:27 +0900

    [Add] routerを作成

--- src/router.js
+++ src/router.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Top from './components/Top.vue';
 import Options from './components/Options.vue';
-import Types from './components/Types.vue';
 import Result from './components/Result.vue';
+import TypesWord from './components/TypesWord.vue';
+import TypesAction from './components/TypesAction.vue';
+import TypesMusic from './components/TypesMusic.vue';
 
 // Vue.use(...)プラグインを適用する
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
-  routes: [{ path: '/', component: Top }, { path: '/options', component: Options }, { path: '/types', component: Types }, { path: '/result', component: Result }]
+  routes: [{ path: '/', component: Top }, { path: '/options', component: Options }, { path: '/types/word', component: TypesWord }, { path: '/types/action', component: TypesAction }, { path: '/types/music', component: TypesMusic }, { path: '/result', component: Result }]
 });
