7e951418e4be13b52ade00d3c9dec353dbf8cf1d
nexceed <matsuda.a@nexceed.com>
2021-05-08 23:05:05 +0900

    [Fix] オプションページのクラス名をv-colに変更し、選択を2つに絞る

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,56 +1,136 @@
 <template>
   <div>
     <LogoHeader></LogoHeader>
-    <div class="option">
-      <div class="option-title">
-        <p>What option do you want?</p>
-      </div>
-      <div class="option-menu">
-        <div class="option-menu-card">
-          <div class="option-menu-card-title">
+    <v-container>
+      <v-row>
+        <v-col class="12 option-title">
+          <p>What option do you want?</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="text-card">
+          <div class="menu-title">
             <p>言葉 - Word</p>
           </div>
-          <div class="option-menu-card-image">
+          <div class="img-card">
             <router-link to="/types/word">
               <img src="@/assets/word.svg">
             </router-link>
           </div>
-          <div class="option-menu-card-description">
+          <div class="menu-description">
             <p>×100の言葉</p>
           </div>
-        </div>
-        <div class="option-menu-card">
-          <div class="option-menu-card-title">
+        </v-col>
+        <v-col class="text-card">
+          <div class="menu-title">
             <p>行動 - Action</p>
           </div>
-          <div class="option-menu-card-image">
+          <div class="img-card">
             <router-link to="/types/action">
               <img src="@/assets/action.svg">
             </router-link>
           </div>
-          <div class="option-menu-card-description">
+          <div class="menu-description">
             <p>×100の行動</p>
           </div>
-        </div>
-        <div class="option-menu-card">
-          <div class="option-menu-card-title">
+        </v-col>
+        <!-- <v-col class="text-card">
+          <div class="menu-title">
             <p>音楽 - Music</p>
           </div>
-          <div class="option-menu-card-image">
+          <div>
             <router-link to="/types/music">
               <img src="@/assets/music.svg">
             </router-link>
           </div>
-          <div class="option-menu-card-description">
+          <div class="menu-description">
             <p>×100の音楽</p>
           </div>
+        </v-col> -->
+      </v-row>
+    </v-container>
+    <!-- <v-container>
+      <v-row class="option">
+        <div class="option-title">
+          <p>What option do you want?</p>
         </div>
-      </div>
-    </div>
+        <div class="option-menu">
+          <v-col class="option-menu-card">
+            <div class="option-menu-card-title">
+              <p>言葉 - Word</p>
+            </div>
+            <div class="option-menu-card-image">
+              <router-link to="/types/word">
+                <img src="@/assets/word.svg">
+              </router-link>
+            </div>
+            <div class="option-menu-card-description">
+              <p>×100の言葉</p>
+            </div>
+          </v-col>
+          <v-col class="option-menu-card">
+            <div class="option-menu-card-title">
+              <p>行動 - Action</p>
+            </div>
+            <div class="option-menu-card-image">
+              <router-link to="/types/action">
+                <img src="@/assets/action.svg">
+              </router-link>
+            </div>
+            <div class="option-menu-card-description">
+              <p>×100の行動</p>
+            </div>
+          </v-col>
+          <v-col class="option-menu-card">
+            <div class="option-menu-card-title">
+              <p>音楽 - Music</p>
+            </div>
+            <div class="option-menu-card-image">
+              <router-link to="/types/music">
+                <img src="@/assets/music.svg">
+              </router-link>
+            </div>
+            <div class="option-menu-card-description">
+              <p>×100の音楽</p>
+            </div>
+          </v-col>
+        </div>
+      </v-row>
+    </v-container> -->
   </div>
 </template>
 
 <style scoped>
+.option-title {
+  font-size: 50px;
+  font-weight: bold;
+  color: #e5855e;
+  text-align: center;
+}
+
+.text-card {
+  text-align: center;
+  background-color: #F9DFD5;
+  margin: 20px;
+  height: 400px;
+}
+
+.menu-title {
+  font-weight: bold;
+  font-size: 40px;
+  color: #5F5F5F;
+}
+
+.img-card {
+  margin: 0 auto;
+  width: 50%;
+}
+
+.menu-description {
+  margin-top: 30px;
+  font-size: 25px;
+}
+
 .option {
   text-align: center;
   color: #e5855e;
@@ -67,14 +147,14 @@
 }
 
 .option-menu-card {
-  margin: 20px 50px;
+  /* margin: 20px 50px; */
   width: 33%;
   height: 400px;
   background-color: #F9DFD5;
 }
 
 .option-menu-card-title {
-  margin: 20px;
+  /* margin: 20px; */
   font-size: 40px;
   color: #5F5F5F;
 }
